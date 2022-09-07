24ccf0be022a37ed32086c2e57878e7dd2e2bd7c
Hayden Hoffman <haydenhoffman@gmail.com>
2022-09-07 19:55:27 -0400

    Handling invalid filetypes and automatic filetype coercion.

--- src/routes/Admin.js
+++ src/routes/Admin.js
@@ -1,4 +1,4 @@
-import {Autocomplete, Checkbox, Fab, IconButton, TextField} from "@mui/material";
+import {Alert, Autocomplete, Checkbox, Fab, IconButton, Snackbar, TextField} from "@mui/material";
 import './Admin.css'
 import {useEffect, useState} from "react";
 import {deleteItemByID, getItems, updateItems} from "../api/firebase";
@@ -9,6 +9,8 @@ export default function Admin() {
     // each item has three properties: id, isVideo, and src
     const [items, setItems] = useState([]);
     const [changedIds, setChangedIds] = useState([]);
+    const [showError, setShowError] = useState(false);
+    const [showSnackbar, setShowSnackbar] = useState(false);
 
     const fetchItems = async () => {
         const items_response = await getItems()
@@ -44,25 +46,28 @@ export default function Admin() {
     // we can track the ids of the image sources we've changed
     const confirmButtonListener = () => {
         let itemsToBeUpdated = [];
+        let invalid_flag_type = [];
+        const SUPPORTED_TYPES = ["jpg", "jpeg", "png", "mp4"];
 
-        // const parseURLAsStatic = (url) => {
-        //     const id = url.split('https://drive.google.com/file/d/')[1].split('/view')[0];
-        //     return `https://drive.google.com/uc?id=${id}`
-        // }
-
+        setShowError(false);
         items.forEach((item) => {
-            changedIds.forEach((changedId) => {
-                if (item.id === changedId) {
 
-                    // parse url from google drive link to its static url
-                    // item.src = parseURLAsStatic(item.src);
+            if (!SUPPORTED_TYPES.some(type => item.src.includes(type))) invalid_flag_type.push(0);
+
+            changedIds.forEach((changedId) => {
+
+                // if item.id is changed id, we will change it
+                if (item.id === changedId) {
                     itemsToBeUpdated.push(item);
                 }
             })
         })
 
+        if (invalid_flag_type.length > 0) return setShowError(true);
         // console.log(itemsToBeUpdated);
         updateItems(itemsToBeUpdated);
+
+        setShowSnackbar(true);
 
         //reset state
         setChangedIds([]);
@@ -90,30 +95,27 @@ export default function Admin() {
     }
 
     const changeTextListener = (newValue, index) => {
+
+        const SUPPORTED_IMG_TYPES = ["jpg", "jpeg", "png"];
+        const SUPPORTED_VID_TYPES = ["mp4"]
+
         // change src at index but also notify react that we've changed this textfield specifically, by tracking its id
         // this is so we can update it in firestore later when we press confirm
         setItems(prevState => {
             let nextState = [...prevState];
             nextState[index].src = newValue;
+
+            // set the isVideo property depending on the filetype of the src
+            if (SUPPORTED_IMG_TYPES.some(type => newValue.includes(type))) {
+                nextState[index].isVideo = false;
+            } else if (SUPPORTED_VID_TYPES.some(type => newValue.includes(type))) {
+                nextState[index].isVideo = true;
+            }
+
             return nextState;
         })
 
         setChangedIdsWrapper(items[index].id);
-    }
-
-    const changeAutocompleteListener = (newValue, index) => {
-        setItems(prevState => {
-            let nextState = [...prevState];
-
-            if (newValue === "Video") {
-                nextState[index].isVideo = true;
-            }
-
-            if (newValue === "Image") {
-                nextState[index].isVideo = false;
-            }
-            return nextState;
-        })
     }
 
     const getTextFields = () => {
@@ -121,17 +123,10 @@ export default function Admin() {
             <div key={index} className="textfield-container">
                 <TextField
                     className="admin-text"
-                    label="Enter your google drive source here"
-                    placeholder="e.g. https://drive.google.com/file/d/1jd4k5qVAZkozDmN7b_h7rKbqVJ8Qvs3-/view"
+                    label="Enter your source link here"
+                    placeholder="e.g. https://i.imgur.com/AT6FqXD.jpeg"
                     value={item.src}
                     onChange={(e) => changeTextListener(e.target.value, index)}
-                />
-                <Autocomplete
-                    className="admin-autocomplete"
-                    renderInput={(params) => <TextField {...params} label="Media type"/>}
-                    options={["Image", "Video"]}
-                    value={item.isVideo ? "Video" : "Image"}
-                    onChange={(e, v) => {changeAutocompleteListener(v, index)}}
                 />
                 <IconButton className="trash" onClick={() => deleteButtonListener(item.id)}>
                     <Delete/>
@@ -154,5 +149,15 @@ export default function Admin() {
             <Check/>
             <p className="icon-button-label">Confirm changes</p>
         </IconButton>
+        <Snackbar open={showSnackbar} autoHideDuration={3000} onClose={() => setShowSnackbar(false)}>
+            <Alert onClose={() => setShowSnackbar(false)} severity="success" sx={{ width: '100%' }}>
+                Your slideshow has been updated!
+            </Alert>
+        </Snackbar>
+        <Snackbar open={showError} autoHideDuration={6000} onClose={() => setShowError(false)}>
+            <Alert onClose={() => setShowError(false)} severity="error" sx={{ width: '50%' }}>
+                Invalid filetype entered in one of the fields. Supported filetypes are .png .jpg .jpeg and .mp4
+            </Alert>
+        </Snackbar>
     </div>)
 }
